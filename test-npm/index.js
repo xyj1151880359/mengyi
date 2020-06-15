44b4a9b7714e1d7c3d3afd8dd8ffccc57d557d88
xyj1151880359 <1151880359@qq.com>
2020-06-15 17:06:26 +0800

    第一次提交

---
+++ test-npm/index.js
@@ -0,0 +1,24 @@
+let obj = {
+    'name':'我是sage'
+}
+/**
+ * 1：会发布流程
+ * 2：总结错误；发布博客
+ * 3：package.json 蹦年写注释 这个文件是核心文件；       
+ * 
+ *  
+ * 任务发布成功
+ * 400   r
+ * 401  邮箱没验证erqwe
+ * 403  登陆失败  naem 名字 为关键字  rqerwqrqrerw
+ * DO YOU MEAT IS these> 命令写shougna zai nalie wjnfei qwerqwqwerwqrweqr
+ * 
+ * 
+ * 注意：
+ * 把错误提示 赋值粘贴 然后 翻译 找到原因 最后解决
+ */
+
+ console.log('yufei is beaiful')
+ 
+
+// module.export = obj
